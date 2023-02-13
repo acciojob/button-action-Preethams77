67d52a80af8e96135832ee973c5b12de3ce1d4cf
Preethams77 <115490278+Preethams77@users.noreply.github.com>
2023-02-13 16:18:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
-    <div id="main">
+    <div id="main" className="bg-pink">
+      {isClicked && (
+        <p>
+          <h1>This is a paragraph</h1>
+        </p>
+      )}
+      <button id="button" onClick={handleClick}>
+        Click me
+      </button>
       // Do not alter the main div
     </div>
   );
 }
-
 
 export default App;
